7582f523268be3cccffbf7dbaf34c74396bda0f5
Scarpy19 <nootravez19@gmail.com>
2025-01-29 14:05:53 +0100

    new btn

--- reactinit/src/App.tsx
+++ reactinit/src/App.tsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Button from './component/CButton/Button.tsx'
-import Card from './component/Card/Card.tsx'
+import viteLogo from './assets/vite.svg'
+import Button from './component/CButton'
+import Card from './component/Card'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [texto, setTexto] = useState("Texto")
 
   function countUp() {
     setCount((count) => count + 1)
+  }
+
+  function changeText() {
+    const newText = prompt("Nuevo texto")
+    if (newText === null) return
+    setTexto(newText)
   }
 
   return (
@@ -22,12 +29,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
         <Card text={`${count}`}>
-          Hola
+          {texto}
         </Card>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
         <Button text="count up" func={countUp} />
+        <Button text="Cambiar texto" func={changeText} />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
